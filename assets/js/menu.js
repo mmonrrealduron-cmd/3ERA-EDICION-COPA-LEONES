7f5d35c50ad57b32d76d53c1f129b44bdecee60f
mmonrrealduron-cmd <mmonrrealduron@gmail.com>
2026-05-10 19:06:20 -0600

    Delete assets/js/menu.js

--- assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('menu-toggle');
-const navbar = document.querySelector('.navbar');
-
-toggle.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
